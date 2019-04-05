1bc65d93dc752b37655eea3b58a45dded9f5ac1d
Daniela <dannyjar.7>
2019-04-04 20:34:33 -0500

    zs
    subir archivo

---
+++ main.py
@@ -0,0 +1,29 @@
+# x = int(input("ingrese dato: "))
+# #print(x)
+# y = int(input("ingrese dato: "))
+# print(x + y)
+# a = True
+# b = False
+# print=(a)
+
+""" a = 5
+if a<4:
+    print("hola")
+    print("mundo")
+else:
+    print("No es menor") """
+
+a=3
+b=4
+if a ==3 and b==4:
+    print("es correcto")
+# a = 7
+# while a==7:
+#     print("estoy en el while")    
+#     a = int(input("ingresa el valor de "))
+#     if a !=7:
+#         break
+
+
+
+
